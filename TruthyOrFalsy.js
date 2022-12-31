b6d23824d67f9487fadb025d740c8734c665b91f
Rakib Ahasan <bd.rakibahasan@gmail.com>
2022-12-31 08:50:42 +0600

    Truthy or Falsy Value

---
+++ TruthyOrFalsy.js
@@ -0,0 +1,11 @@
+//Truthy Falsy
+var myVar = true;
+//False, 0, null, undefined, nan, ""-> then false otherwise true 
+if(myVar)   // myVar === true;
+{
+    console.log("I am truthy");
+}
+else
+{
+    console.log("I am falsy");
+}
